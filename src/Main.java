4cbea6dc1b893799c796d4fc95bd961ae384343a
daniel19066 <dpallaresr@unal.edu.co>
2020-02-02 15:24:37 -0500

    algo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import grammar.lengBasicLexer;
+import grammar.lengBasicParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 import javax.swing.*;
 import java.io.IOException;
+import java.io.File;
 
 public class Main extends JFrame{
     public Main() throws IOException {
@@ -10,18 +15,23 @@ public class Main extends JFrame{
 
         dibujo.Variables();
         dibujo.Funciones();
-/*
-        fortranLexer lexer;
-        if (args.length>0)
-            lexer = new fortranLexer(CharStreams.fromFileName(args[0]));
-        else
-            lexer = new fortranLexer(CharStreams.fromStream(System.in));
-        CommonTokenStream tokens = new CommonTokenStream((TokenSource) lexer);
-        fortranParser parser = new fortranParser((TokenStream) tokens);
-        ParseTree tree = parser.begin();
 
-        LEX loader = new LEX();
-        loader.visit(tree);*/
+        lengBasicLexer lexer;
+        if (args.length>0)
+            lexer = new lengBasicLexer(CharStreams.fromFileName(args[0]));
+        else
+            lexer = new lengBasicLexer(CharStreams.fromStream(System.in));
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        lengBasicParser parser = new lengBasicParser(tokens);
+        ParseTree tree = parser.begin(); // begin parsing at init rule
+
+        // Create a generic parse tree walker that can trigger callbacks
+        ParseTreeWalker walker = new ParseTreeWalker();
+        // Walk the tree created during the parse, trigger callbacks
+        walker.walk(new Short_to_unicode(), tree);
+        System.out.println(); // print a \n after translation
     }
 }
 
